4cc2cf7c88e8de77dbd50c4d0204e7259d95e1bf
Gustavo Nascimento Souza <gustavo.ns@outlook.com.br>
2022-10-09 15:59:36 -0300

    Aula 05 (Mobile) - :heavy_plus_sign: Added Dependencies (expo-clipboard, expo-modules-core, expo-notifications); :memo: Create components (DuoMatch) and Update 'Game' screen; :wrench: Added 'getPushNotificationToken.ts' and 'notificationConfigs.ts'

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,18 +1,50 @@
+import { useEffect, useRef } from "react";
 import { StatusBar } from "react-native";
+
 import {
   useFonts,
   Montserrat_400Regular,
   Montserrat_600SemiBold,
   Montserrat_700Bold,
   Montserrat_900Black,
-} from "@expo-google-fonts/montserrat"
+} from "@expo-google-fonts/montserrat";
+
+import { Subscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
 
 import { Routes } from './src/routes';
 import { Background } from "./src/components/Background/index";
 import { Loading } from "./src/components/Loading/index";
 import React from "react";
 
+import './src/service/notificationConfigs';
+import { getPushNotificationToken } from "./src/service/getPushNotificationToken";
+
 export default function App() {
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken()
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    });
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  }, []);
+
   const [fontsLoaded] = useFonts({
     Montserrat_400Regular,
     Montserrat_600SemiBold,
